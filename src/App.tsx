0eeff4e13cb4ccb11bd18b37d487018f42d78f64
hadardv <hadardv14@gmail.com>
2025-09-06 12:18:54 +0300

    Remove large GLB and ignore model files

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import DeskSetupViewer from "./components/DeskSetupViewer";
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <DeskSetupViewer src="/models/setup.glb" autoRotate helpers={false} />
+      <DeskSetupViewer
+        src="/models/gaming_setup_low-poly.glb"
+        autoRotate
+        helpers={false}
+      />
     </div>
   );
 }
