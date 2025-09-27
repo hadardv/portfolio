d13667d904581c4908da88e579e5e493f22a4775
hadardv <hadardv14@gmail.com>
2025-09-28 01:09:14 +0300

    fixed dependency array

--- src/components/DeskSetupViewer.tsx
+++ src/components/DeskSetupViewer.tsx
@@ -195,7 +195,7 @@ export default function DeskSetupViewer({
       const canvas = renderer.domElement;
       canvas.parentElement?.removeChild(canvas);
     };
-  }, [src, autoRotate, helpers, fit, interactive, hideByName.join(",")]);
+  }, [src, autoRotate, helpers, fit, interactive, hideByName]);
 
   return (
     <div
